f28a2ed70081aa9ec5d2519f1cd4425c3b41e6cb
Fionn Fitzmaurice <fionn@github.com>
2025-03-29 12:19:33 +0800

    Add rule to detect plaintext remote redirect URIs

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 			Rules: []tflint.Rule{
 				rules.NewOktaPolicyNameRule(),
 				rules.NewOktaAppOauthOmitSecretRule(),
+				rules.NewOktaAppOauthPlaintextRedirectURIRule(),
 			},
 		},
 	})

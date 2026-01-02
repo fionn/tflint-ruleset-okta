4010a3f50a9171caf2e34b74a588681dbc47b6a5
Fionn Fitzmaurice <fionn@github.com>
2026-01-02 21:47:11 +0800

    Retroactively bump ruleset version
    Fixup-for: ac039c20bbc53f95094f9166fd8e33086a9d0c7e (v0.1.10)

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
 			Name:    "okta",
-			Version: "0.1.9",
+			Version: "0.1.10",
 			Rules: []tflint.Rule{
 				rules.NewOktaPolicyNameRule(),
 				rules.NewOktaAppOauthOmitSecretRule(),
